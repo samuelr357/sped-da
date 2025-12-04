ecfa187cc481fcaa77a606e732107fa49d6f9190
Samuel Rodrigues <Samuel.oficial7@gmail.com>
2025-12-04 10:31:16 -0300

    update ShopeeProcessor Order

--- src/Shopee/EtiquetaShopeeProcessor.php
+++ src/Shopee/EtiquetaShopeeProcessor.php
@@ -7,57 +7,6 @@ use setasign\Fpdi\Fpdi;
 use setasign\Fpdi\PdfParser\StreamReader;
 
 /**
- * Classe para permitir rotação (extensão do FPDI/FPDF)
- */
-class PDF_Rotate extends Fpdi
-{
-    protected $angle = 0;
-
-    public function Rotate($angle, $x = -1, $y = -1)
-    {
-        if ($x == -1)
-            $x = $this->x;
-        if ($y == -1)
-            $y = $this->y;
-
-        if ($this->angle != 0) {
-            $this->_out('Q');
-        }
-
-        $this->angle = $angle;
-
-        if ($angle != 0) {
-            $angle *= M_PI / 180;
-            $c = cos($angle);
-            $s = sin($angle);
-            $cx = $x * $this->k;
-            $cy = ($this->h - $y) * $this->k;
-
-            $this->_out(sprintf(
-                'q %.5F %.5F %.5F %.5F %.5F %.5F cm 1 0 0 1 %.5F %.5F cm',
-                $c,
-                $s,
-                -$s,
-                $c,
-                $cx,
-                $cy,
-                -$cx,
-                -$cy
-            ));
-        }
-    }
-
-    protected function _endpage()
-    {
-        if ($this->angle != 0) {
-            $this->angle = 0;
-            $this->_out('Q');
-        }
-        parent::_endpage();
-    }
-}
-
-/**
  * Processador de etiquetas Shopee + DANFE
  */
 class EtiquetaShopeeProcessor
@@ -189,5 +138,56 @@ class EtiquetaShopeeProcessor
         }
 
         return $pdfFinal->Output('S');
+    }
+}
+
+/**
+ * Classe para permitir rotação (extensão do FPDI/FPDF)
+ */
+class PDF_Rotate extends Fpdi
+{
+    protected $angle = 0;
+
+    public function Rotate($angle, $x = -1, $y = -1)
+    {
+        if ($x == -1)
+            $x = $this->x;
+        if ($y == -1)
+            $y = $this->y;
+
+        if ($this->angle != 0) {
+            $this->_out('Q');
+        }
+
+        $this->angle = $angle;
+
+        if ($angle != 0) {
+            $angle *= M_PI / 180;
+            $c = cos($angle);
+            $s = sin($angle);
+            $cx = $x * $this->k;
+            $cy = ($this->h - $y) * $this->k;
+
+            $this->_out(sprintf(
+                'q %.5F %.5F %.5F %.5F %.5F %.5F cm 1 0 0 1 %.5F %.5F cm',
+                $c,
+                $s,
+                -$s,
+                $c,
+                $cx,
+                $cy,
+                -$cx,
+                -$cy
+            ));
+        }
+    }
+
+    protected function _endpage()
+    {
+        if ($this->angle != 0) {
+            $this->angle = 0;
+            $this->_out('Q');
+        }
+        parent::_endpage();
     }
 }
--- tests/Shopee/ShopeeEtiqueta.php
+++ tests/Shopee/ShopeeEtiqueta.php
@@ -1,7 +1,6 @@
 <?php
 
 require __DIR__ . '/../../../vendor/autoload.php';
-require __DIR__ . '/../../src/Shopee/EtiquetaShopeeProcessor.php';
 
 $files = [
     [
